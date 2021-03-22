a39184ac9eb38b2762e88f88fc538a7c11561002
Cora Jacobson <cora-jacobson@lambdastudents.com>
2021-03-21 18:06:47 -0700

    UI Hookups and minor updates

---
+++ labs-ios-starter/PropertyData.swift
@@ -0,0 +1,15 @@
+//
+//  PropertyData.swift
+//  labs-ios-starter
+//
+//  Created by Cora Jacobson on 3/21/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+struct PropertyData {
+    var propertyLabel: String
+    var valueLabel: String
+    var percentage: Float
+}
