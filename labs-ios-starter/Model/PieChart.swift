fd872e97cb8ca2d333d99d0e29bbbf872a0b77f8
Kstu24 <Kistewart419@gmail.com>
2021-04-18 22:21:53 -0400

    Settings view completed

---
+++ labs-ios-starter/Model/PieChart.swift
@@ -0,0 +1,17 @@
+//
+//  PieChart.swift
+//  labs-ios-starter
+//
+//  Created by Kevin Stewart on 4/16/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PieChartSegment {
+    var color: UIColor
+    var value: CGFloat
+    
+    static let savedSegment = "SavedSegment"
+}
